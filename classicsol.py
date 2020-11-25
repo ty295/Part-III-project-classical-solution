1c32111a7e92c5ddad41a8f0417fc02e1148682f
Thomas Young <thomasdyoung1998@gmail.com>
2020-11-25 16:04:44 +0000

    Antistokes regime has been implemented. The solutions in the antistokes regime for different cubic coefficients have compared in a subplot.

--- classicsol.py
+++ classicsol.py
@@ -32,20 +32,47 @@ class ODE_solver():
         return self.time_eval
 
 
+def stokes_regime(initial,times,drag_coeff,lin_coeff):
+    def force(x,v):
+        return -drag_coeff*v - lin_coeff*x
+    sol = ODE_solver(initial,force,times)
+    plt.plot(sol.get_times(),sol.get_positions(),marker = '')
+    plt.show()
+
+def antistokes_regime(initial,times,drag_coeff,lin_coeff,cub_coeff):
+    def force(x,v):
+        return drag_coeff*v - lin_coeff*x - cub_coeff*(x**3)
+    sol = ODE_solver(initial,force,times)
+    return sol
+
+
+def antistokes_regime_cub_plot(initial,times,drag_coeff,lin_coeff,cub_coeffs):
+    fig, ax = plt.subplots(len(cub_coeffs),sharex = True)
+    for i in range(len(cub_coeffs)):
+        cub_coeff = cub_coeffs[i]
+        sol = antistokes_regime(initial,times,drag_coeff,lin_coeff,cub_coeff)
+        ax[i].plot(sol.get_times(),sol.get_positions(),marker = '')
+        ax[i].set_title('cub_coeff = {}'.format(cub_coeff))
+    plt.xlabel('Time')
+    plt.ylabel('Position')
+    plt.show()
+    fig.tight_layout()
+
+
+
 
 initial = [1,0]
-times = np.linspace(0,20,1000)
+times = np.linspace(0,100,1000)
 
-def force(x,v):
-    return -v - 5*x
-
-
+'''
 solve = ODE_solver(initial,force,times)
 plt.plot(solve.get_times(),solve.get_positions(),marker = '')
 plt.show()
+'''
 
+cub_coeffs = [100,50,20,10]
 
-
+x = antistokes_regime_cub_plot(initial,times,0.05,1,cub_coeffs)
 
 
 
